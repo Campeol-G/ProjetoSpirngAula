757ea36105bd02dcfce90db73d71ca1f2fa2192f
ChicoMagnusOpus <gustavocampeol28@gmail.com>
2026-07-01 20:02:16 -0300

    User entity and resource

--- src/main/java/com/primeiroP/ProjetoEstudo/ProjetoEstudoApplication.java
+++ src/main/java/com/primeiroP/ProjetoEstudo/ProjetoEstudoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoEstudoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoEstudoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ProjetoEstudoApplication.class, args);
+  }
 
 }
