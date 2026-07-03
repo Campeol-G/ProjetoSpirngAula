d62af23288eb0711782c4f5a9796275ffa42d33c
ChicoMagnusOpus <gustavocampeol28@gmail.com>
2026-07-03 19:20:43 -0300

    service layer, component registration

--- src/main/java/com/primeiroP/ProjetoEstudo/resources/UserResource.java
+++ src/main/java/com/primeiroP/ProjetoEstudo/resources/UserResource.java
@@ -1,19 +1,38 @@
 package com.primeiroP.ProjetoEstudo.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.primeiroP.ProjetoEstudo.entities.User;
+import com.primeiroP.ProjetoEstudo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+  @Autowired
+  private UserService service;
+
   @GetMapping
-  public ResponseEntity<User> findAll() {
-    User u = new User(1, "Joao", "Joao@gmail.com", "999299292", "fortnite");
+  public ResponseEntity<List<User>> findAll() {
+    List<User> list = service.findAll();
+    return ResponseEntity.ok().body(list);
+  }
+
+  // o value pra dizer q a URL vai ter um parametro
+  // a requisicao aceita um ID dentro da URL
+  @GetMapping(value = { "/{id}" })
+  public ResponseEntity<User> findByID(@PathVariable Integer id) {
+    // o path PathVariable eh pra fazer essa funcao pegar o id da URL e usar pra
+    // fazer a busca no DB
+    User u = service.findById(id);
     return ResponseEntity.ok().body(u);
   }
 }
---
+++ src/main/java/com/primeiroP/ProjetoEstudo/services/UserService.java
@@ -0,0 +1,26 @@
+package com.primeiroP.ProjetoEstudo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.primeiroP.ProjetoEstudo.entities.User;
+import com.primeiroP.ProjetoEstudo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository repository;
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
+
+  public User findById(Integer id) {
+    return repository.findById(id).get();
+    // o .get no final dessa expressao serve pra rentornar o tipo do Optional
+  }
+}
